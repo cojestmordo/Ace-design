d582aaf1e4e062909a1b0d98eeafe5e40eefb3a7
cojestmordo <lordmada2012@gmail.com>
2022-01-25 18:10:31 +0100

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+tutaj dodam krótki opis o mnie</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+        color:white;
+		width:100%
+	}
+
+</style>
