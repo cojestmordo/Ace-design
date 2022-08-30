04e97e684e4c36af75fdf16deb21ca73f1bcc468
Mada <lordmada2012@gmail.com>
2022-08-31 01:14:10 +0200

    added front page grid

--- client/src/components/FrontPage.vue
+++ client/src/components/FrontPage.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-    <div>this is front page</div>
-
-    <div>hello World</div>
+  <div class='grid-container'>
+    <div class='foto item' >
+      <p>foto</p>
+    </div>
+    <div class='design item'>
+      <p>design</p>
+    </div>
+    <div class='poster item'>
+      <p>poster</p>
+    </div>
+    <div class='info item'>
+      <p>info</p>
+    </div>
   </div>
 </template>
 
@@ -11,3 +20,42 @@
     name:"FrontPage"
   }
 </script>
+
+<style scoped>
+.item{
+  width:25vw;
+  height:30vh;
+  color:white;
+  background-color:black;
+  display:flex;
+}
+.item p{
+  font-size:10vh;
+  margin:auto;
+  text-align:center;
+  vertical-align: middle;
+}
+.grid-container{
+  width:100vw;
+  height:100vh;
+  gap:2%;
+  display:grid;
+  align-content: space-evenly;
+  align-items: center;
+  justify-content: space-evenly;
+  justify-items:center;
+}
+.foto{
+  grid-column: 1/2;
+}
+.design{
+  grid-column: 2/3;
+}
+.poster{
+  grid-column: 1/2;
+}
+.info{
+  grid-column: 2/3;
+}
+
+</style>
